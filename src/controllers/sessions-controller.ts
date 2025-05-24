9c009682646f130093a443dd3e6a783743dac85b
Horquidia <horquidia.a.lima@gmail.com>
2025-05-24 20:23:20 -0300

    Criando Sessão

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,7 +1,33 @@
 import { Request, Response } from "express"
+import { AppError } from "@/utils/AppError"
+import { authConfig } from "@/configs/auth"
+import { sign } from "jsonwebtoken"
+
 
 class SessionsController {
-  async create(request: Request, response: Response) {}
+  async create(request: Request, response: Response) {
+
+    const {username, password} = request.body
+
+    const fakeUser = {
+      id:1,
+      username: "quida",
+      password: "123456",
+    }
+
+    if(username !== fakeUser.username || password !== fakeUser.password){
+      throw new AppError("Username e/ou senha invalido", 401)
+    }
+
+    const {secret, expiresIn} = authConfig.jwt
+
+    const token = sign({},secret, {
+      expiresIn,
+      subject : String(fakeUser.id),
+    })
+
+    return response.json({token})
+  }
 }
 
 export { SessionsController }
