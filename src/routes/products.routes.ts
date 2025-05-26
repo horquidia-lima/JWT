3c27e24c8731bce569fbff8305c197cb70ad3080
Horquidia <horquidia.a.lima@gmail.com>
2025-05-25 21:35:11 -0300

    Middleware de Autorização

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -2,11 +2,14 @@ import { Router } from "express"
 import { ProductsController } from "@/controllers/products-controller"
 
 import { ensureAuthenticated } from "@/middleware/ensureAuthenticated"
+import { verifyUserAuthorization } from "@/middleware/verifyUserAuthorization"
 
 const productsRoutes = Router()
 const productsController = new ProductsController()
 
+//Todas as totas productsRoutes.use(verifyUserAuthorization(["sale", "admin"]))
+
 productsRoutes.get("/", productsController.index)
-productsRoutes.post("/", ensureAuthenticated,productsController.create)
+productsRoutes.post("/", ensureAuthenticated, verifyUserAuthorization(["sale", "admin"]),productsController.create)
 
 export { productsRoutes }
